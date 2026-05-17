1d56cde88bad5409105d1b305ff26a7a3002117a
Luisa <luisaisraelnobregacosta@gmail.com>
2026-05-17 17:12:09 -0300

    feat: adiciona tela login

--- CinePedia/src/app/app.ts
+++ CinePedia/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuNavegacao } from "./shared/components/menu-navegacao/menu-navegacao";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuNavegacao],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
