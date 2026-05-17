7caf8f6d9769f809919b513c36462ff2d6aaffef
Luisa <luisaisraelnobregacosta@gmail.com>
2026-05-17 16:43:28 -0300

    feat: adiciona menu de navegação

--- CinePedia/src/app/app.routes.ts
+++ CinePedia/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Listagem } from './pages/listagem/listagem';
 import { Login } from './pages/login/login';
 import { Cadastro } from './pages/cadastro/cadastro';
 import { HomePage } from './pages/home-page/home-page';
+import { Perfil } from './pages/perfil/perfil';
+import { Relatorio } from './pages/relatorio/relatorio';
 
 export const routes: Routes = [
-  { path: '', component: HomePage },  //onde cada objeto define a URL (path) e qual componente deve ser renderizado
-  { path: 'cadastro', component: Cadastro },
+  { path: '', component: HomePage }, 
+  { path: 'cadastro', component: Cadastro },//Quando a URL for /cadastro, carregue o componente cadastro.
   { path: 'login', component: Login },
-  { path: 'filmes', component: Listagem},
+  { path: 'listagem', component: Listagem},
+  { path: 'perfil', component: Perfil },
+  { path: 'relatorio', component: Relatorio },
 ];
